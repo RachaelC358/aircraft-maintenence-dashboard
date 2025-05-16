abb5c1c809f32e11792c94c81e78c9326e90d524
Rachael Carpenter <carprl@outlook.com>
2025-05-16 15:17:46 -0500

    add script: count rows

---
+++ Part2RealDataSet/scripts/findTotalTasks.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+xlsx_path = r"C:\Users\carpr\OneDrive\Desktop\work\aircraft-maintenence-dashboard\Part2RealDataSet\data\Task Allocation Input 2018-2021.xlsx"
+
+# Load your Excel file
+df = pd.read_excel(xlsx_path, sheet_name="Tasks") 
+
+# Count total number of tasks (rows)
+total_tasks = len(df)
+
+print(f"Total number of recorded maintenance tasks: {total_tasks}")
